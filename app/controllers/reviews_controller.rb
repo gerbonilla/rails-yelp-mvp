e6808289dbb7158d911bb39ab5149615595fa2d5
Gerardo Bonilla <gerbonilla@gmail.com>
2017-08-16 17:13:50 +0200

    yelp mvp completed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,21 @@
 class ReviewsController < ApplicationController
-  def create
-  end
 
   def new
+    @review = Review.new()
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    @review.save
+    redirect_to restaurant_path(@review.restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,17 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = %w(chinese italian japanese french belgian)
+
   has_many :reviews, dependent: :destroy
   validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: %w(chinese italian japanese french belgian), message: "%{value} is not a valid category" }
+
+  def average_rating
+    reviews = self.reviews.all
+    average = 0
+    reviews.each do |review|
+      average += review.rating
+    end
+    average = average / reviews.length unless reviews.length.zero?
+    return average
+  end
 end
