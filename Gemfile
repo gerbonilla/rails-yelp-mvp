a4db50150a0a396ee49fd0f7ad2f8b103bcf19a5
Gerardo Bonilla <gerbonilla@gmail.com>
2017-08-16 12:06:23 +0200

    routes, controllers, and models are all set up. Missing methods and html

--- Gemfile
+++ Gemfile
@@ -18,6 +18,11 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
+gem 'bootstrap-sass'
+gem 'font-awesome-sass'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+gem 'jquery-rails'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
@@ -38,12 +43,14 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+
 end
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '~> 3.0.5'
+  gem 'pry-rails'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating Restaurants"
+
+Restaurant.create!({
+  name: "Stadtklause",
+  address: "Bernburger Str. 35",
+  phone_number: "030 51056381",
+  category: %w(chinese italian japanese french belgian).sample
+  })
+
+Restaurant.create!({
+  name: "Gustav & Gold",
+  address: "Stresemannstr. 48 - 52",
+  phone_number: "030 25936925",
+  category: %w(chinese italian japanese french belgian).sample
+  })
+
+Restaurant.create!({
+  name: "Restaurant Bastard",
+  address: "Reichenberger Str. 122",
+  phone_number: "030 54821866",
+  category: %w(chinese italian japanese french belgian).sample
+  })
+
+Restaurant.create!({
+  name: "Glashaus",
+  address: "Lindenstr. 29",
+  phone_number: "030 2518491",
+  category: %w(chinese italian japanese french belgian).sample
+  })
+
+Restaurant.create!({
+  name: "Max & Moritz",
+  address: "Oranienstr. 162",
+  phone_number: "030 69515911",
+  category: %w(chinese italian japanese french belgian).sample
+  })
+
+puts "Restaurants created!"
